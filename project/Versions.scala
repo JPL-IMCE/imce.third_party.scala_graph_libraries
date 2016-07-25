aa3cf5dcfa40d964e54bea446baac16ba23d01a3
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-07-25 00:15:36 +0000

    IMCEI-272 - Add connector to dump OTI MOF profiles/models for SPARK dependencies updates

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,21 +2,21 @@
 object Versions {
 
   // this project's version
-  val version = "2.4.0"
-  val scala_version = "2.11.7"
+  val version = "2.5.0"
+  val scala_version = "2.11.8"
 
   // graphLibs
 
   // http://mvnrepository.com/artifact/com.assembla.scala-incubator/graph-core_2.11
-  val graph_core = "1.10.0"
+  val graph_core = "1.11.0"
 
   // http://mvnrepository.com/artifact/com.assembla.scala-incubator/graph-constrained_2.11
-  val graph_constrained = "1.10.0"
+  val graph_constrained = "1.11.0"
 
   // http://mvnrepository.com/artifact/com.assembla.scala-incubator/graph-dot_2.11
-  val graph_dot = "1.10.1"
+  val graph_dot = "1.11.0"
 
   // http://mvnrepository.com/artifact/com.assembla.scala-incubator/graph-json_2.11
-  val graph_json = "1.10.0"
+  val graph_json = "1.11.0"
 
 }
